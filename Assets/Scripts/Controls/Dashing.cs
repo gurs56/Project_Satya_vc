a4ab83bb0e3ab1040d96836be9e4542880a5f1ef
Electroshockist <ericjohnbalas@gmail.com>
2025-05-05 02:46:29 -0400

    Added a bunch o' playercontrolller tests

---
+++ Assets/Scripts/Controls/Dashing.cs
@@ -0,0 +1,119 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Dashing : MonoBehaviour
+{
+    [Header("References")]
+    public Transform orientation;
+    public Transform playerCam;
+    private Rigidbody rb;
+    private PlayerMove pm;
+
+    [Header("Dashing")]
+    public float dashForce;
+    public float dashUpwardForce;
+    public float maxDashYSpeed;
+    public float dashDuration;
+
+    [Header("CameraEffects")]
+    public tpc cam;
+    public float dashFov;
+
+    [Header("Settings")]
+    public bool useCameraForward = true;
+    public bool allowAllDirections = true;
+    public bool disableGravity = false;
+    public bool resetVel = true;
+
+    [Header("Cooldown")]
+    public float dashCd;
+    private float dashCdTimer;
+
+    [Header("Input")]
+    public KeyCode dashKey = KeyCode.E;
+
+    private Vector3 delayedForceToApply;
+
+    private void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+        pm = GetComponent<PlayerMove>();
+    }
+
+    private void Update()
+    {
+        if (Input.GetKeyDown(dashKey))
+            Dash();
+
+        if (dashCdTimer > 0)
+            dashCdTimer -= Time.deltaTime;
+    }
+
+    private void Dash()
+    {
+        if (dashCdTimer > 0) return;
+        else dashCdTimer = dashCd;
+
+        pm.dashing = true;
+        pm.maxYSpeed = maxDashYSpeed; // Allow a higher vertical speed cap during dash
+
+        // Adjust camera FOV for dash effect.
+        cam.DoFov(dashFov);
+
+        Transform forwardT;
+        if (useCameraForward)
+            forwardT = playerCam;
+        else
+            forwardT = orientation;
+
+        Vector3 direction = GetDirection(forwardT);
+        Vector3 forceToApply = direction * dashForce + orientation.up * dashUpwardForce;
+
+        if (disableGravity)
+            rb.useGravity = false;
+
+        delayedForceToApply = forceToApply;
+        Invoke(nameof(DelayedDashForce), 0.025f);
+
+        Invoke(nameof(ResetDash), dashDuration);
+    }
+
+    private void DelayedDashForce()
+    {
+        if (resetVel)
+            rb.linearVelocity = Vector3.zero;
+
+        rb.AddForce(delayedForceToApply, ForceMode.Impulse);
+    }
+
+    private void ResetDash()
+    {
+        pm.dashing = false;
+        pm.maxYSpeed = 0; // Reset vertical speed cap
+
+        // Reset camera FOV to a default value (85f in this example)
+        cam.DoFov(85f);
+
+        if (disableGravity)
+            rb.useGravity = true;
+    }
+
+    private Vector3 GetDirection(Transform forwardT)
+    {
+        float horizontalInput = Input.GetAxisRaw("Horizontal");
+        float verticalInput = Input.GetAxisRaw("Vertical");
+
+        Vector3 direction = new Vector3();
+
+        if (allowAllDirections)
+            direction = forwardT.forward * verticalInput + forwardT.right * horizontalInput;
+        else
+            direction = forwardT.forward;
+
+        if (horizontalInput == 0 && verticalInput == 0)
+            direction = forwardT.forward;
+
+        return direction.normalized;
+    }
+}
---
+++ Assets/Scripts/Controls/PlayerInputManager.cs
@@ -0,0 +1,44 @@
+using UnityEngine;
+using UnityEngine.Events;
+using UnityEngine.InputSystem;
+
+public class PlayerInputManager : MonoBehaviour {
+    [SerializeField]
+    private PlayerInputActions playerInputActions;
+
+    [SerializeField]
+    InputAction action;
+
+    public PlayerInputActions PlayerInputActions {
+        get {
+            return playerInputActions;
+        }
+    }
+
+    public PlayerInputActions.PlayerActions Player {
+        get {
+            return playerInputActions.Player;
+        }
+    }
+
+    public InputAction Motion {
+        get {
+            return Player.Move;
+        }
+    }
+
+    private void OnEnable() {
+        playerInputActions.Enable();
+    }
+
+    private void OnDisable() {
+        playerInputActions.Disable();
+    }
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Awake() {
+        playerInputActions = new PlayerInputActions();
+    }
+
+
+}
